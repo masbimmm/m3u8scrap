5808edec9da9d5ade3743d94678bc9733a8d9e8e
Masbimmm <74353242+masbimmm@users.noreply.github.com>
2024-10-26 00:09:27 +0700

    update test from shortwave download
    baru download filmnya aja, belom merge subttitle

--- app.js
+++ app.js
@@ -1,134 +1,178 @@
 const ffmpeg = require('fluent-ffmpeg');
 const axios = require('axios');
-const fs = require('fs').promises; // Menggunakan fs.promises untuk operasi asinkron
+const fs = require('fs').promises;
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
 app.use('/shortwave', require('./router/shortwave'));
 
-app.listen(3000, ()=>{
-    console.log("serv runn")
-})
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
-const data = [{"chapter_id":"6718ebe070818cb6ab0964aa","chapter_index":1,"chapter_name":"E01","is_free":1},{"chapter_id":"6718eeab12a132519acafdc8","chapter_index":2,"chapter_name":"E02","is_free":1}];
+io.on('connection', (socket) => {
+    const dataddd = [
+        {"chapter_id":"6718ebe070818cb6ab0964aa","chapter_index":1,"chapter_name":"E01","is_free":1},
+        {"chapter_id":"6718eeab12a132519acafdc8","chapter_index":2,"chapter_name":"E02","is_free":1}
+    ];
 
-async function downloadM3U8(url) {
-    const response = await axios.get(url);
-    return response.data;
-}
-
-async function downloadFile(url, filename) {
-    const response = await axios.get(url, {
-        responseType: 'arraybuffer',
-    });
-    await fs.writeFile(filename, response.data);
-    return filename;
-}
-
-async function mergeVideos(m3u8Url, chapterIndex) {
-    const m3u8Data = await downloadM3U8(m3u8Url);
-    const urls = [];
-    const lines = m3u8Data.split('\n');
-
-    lines.forEach((line) => {
-        if (line.startsWith('http')) {
-            urls.push(line.trim());
-        }
-    });
-
-    const segments = [];
-
-    for (let i = 0; i < urls.length; i++) {
-        console.log(`PROGRESS => segment_${chapterIndex}_${i}.ts`)
-        const segmentPath = path.join(__dirname, `segment_${chapterIndex}_${i}.ts`);
-        await downloadFile(urls[i], segmentPath);
-        segments.push(segmentPath);
+    async function downloadM3U8(url) {
+        const response = await axios.get(url);
+        return response.data;
     }
 
-    const listFilePath = path.join(__dirname, `video_list_${chapterIndex}.txt`);
-    await fs.writeFile(listFilePath, segments.map(segment => `file '${path.basename(segment)}'`).join('\n'));
-
-    const outputFile = path.join(__dirname, `${chapterIndex}.mp4`);
-    return new Promise((resolve, reject) => {
-        ffmpeg()
-            .input(listFilePath)
-            .inputOptions('-f', 'concat')
-            .outputOptions([
-                '-c', 'copy',
-                '-movflags +faststart',
-                '-f mp4'
-            ])
-            .on('end', async () => {
-                console.log(`[DONE] => video ${chapterIndex}.mp4`);
-                await fs.unlink(listFilePath);
-                await Promise.all(segments.map(segment => fs.unlink(segment)));
-                resolve(); 
-            })
-            .on('error', (err) => {
-                console.error(`[FAIL] => video ${chapterIndex}.mp4:`, err);
-                reject(err);
-            })
-            .save(outputFile);
-    });
-}
-const limit = (fn, concurrency) => {
-    const queue = [];
-    let activeCount = 0;
-
-    const next = async () => {
-        if (queue.length === 0 || activeCount >= concurrency) return;
-
-        activeCount++;
-        const { fn: queuedFn, resolve, reject } = queue.shift();
-
-        try {
-            const result = await queuedFn();
-            resolve(result);
-        } catch (error) {
-            reject(error);
-        } finally {
-            activeCount--;
-            next(); // Panggil fungsi next untuk memproses berikutnya
-        }
-    };
-
-    return (fn) => {
-        return new Promise((resolve, reject) => {
-            queue.push({ fn, resolve, reject });
-            next();
+    async function downloadFile(url, filename) {
+        const response = await axios.get(url, {
+            responseType: 'arraybuffer',
         });
-    };
-};
+        await fs.writeFile(filename, response.data);
+        return filename;
+    }
 
-const limitedMergeVideos = limit(async (playUrl, chapterIndex) => {
-    return mergeVideos(playUrl, chapterIndex);
-}, 5); 
+    async function mergeVideos(m3u8Url, chapterIndex, channel, namaFilm, filmId) {
+        const dirPath = path.join(__dirname, channel);
+        ensureDirectoryExists(dirPath);
+        const savedirPath = path.join(dirPath, namaFilm);
+        ensureDirectoryExists(savedirPath);
+        const m3u8Data = await downloadM3U8(m3u8Url);
+        const urls = [];
+        const lines = m3u8Data.split('\n');
 
-async function loadVideoList(chapterData) {
-    const promises = chapterData.map(async (chapter) => {
-        let config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: `http://localhost/shortwave/generatechunk.php?drama_id=6707d2ed4686383c41677a27&chapter_id=${chapter.chapter_id}`,
-            headers: {
-                // ...
+        lines.forEach((line) => {
+            if (line.startsWith('http')) {
+                urls.push(line.trim());
+            }
+        });
+
+        const segments = [];
+
+        for (let i = 0; i < urls.length; i++) {
+            socket.emit('progres', {filmId:filmId, msg:`PROGRESS => segment_${chapterIndex}_${i}.ts`})
+            console.log('progres', {filmId:filmId, msg:`PROGRESS => segment_${chapterIndex}_${i}.ts`})
+            const segmentPath = path.join(savedirPath, `segment_${chapterIndex}_${i}.ts`);
+            await downloadFile(urls[i], segmentPath);
+            segments.push(segmentPath);
+        }
+
+        const listFilePath = path.join(savedirPath, `video_list_${chapterIndex}.txt`);
+        await fs.writeFile(listFilePath, segments.map(segment => `file '${path.basename(segment)}'`).join('\n'));
+
+        const outputFile = path.join(savedirPath, `${chapterIndex}.mp4`);
+        return new Promise((resolve, reject) => {
+            ffmpeg()
+                .input(listFilePath)
+                .inputOptions('-f', 'concat')
+                .outputOptions([
+                    '-c', 'copy',
+                    '-movflags +faststart',
+                    '-f mp4'
+                ])
+                .on('end', async () => {
+                    socket.emit('done', {filmId:filmId, msg:`[DONE] => video ${chapterIndex}.mp4`})
+                    await fs.unlink(listFilePath);
+                    await Promise.all(segments.map(segment => fs.unlink(segment)));
+                    resolve(); 
+                })
+                .on('error', (err) => {
+                    socket.emit('fail', {filmId:filmId, msg:`[FAIL] => video ${chapterIndex}.mp4`}, err)
+                    console.error(`[FAIL] => video ${chapterIndex}.mp4:`, err);
+                    reject(err);
+                })
+                .save(outputFile);
+        });
+    }
+
+    const limit = (fn, concurrency) => {
+        const queue = [];
+        let activeCount = 0;
+
+        const next = async () => {
+            if (queue.length === 0 || activeCount >= concurrency) return;
+
+            activeCount++;
+            const { fn: queuedFn, resolve, reject } = queue.shift();
+
+            try {
+                const result = await queuedFn();
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            } finally {
+                activeCount--;
+                next();
             }
         };
 
-        try {
-            const response = await axios.request(config);
-            const playUrl = response.data.data.play_url;
+        return (fn) => {
+            return new Promise((resolve, reject) => {
+                queue.push({ fn, resolve, reject });
+                next();
+            });
+        };
+    };
 
-            return limitedMergeVideos(() => mergeVideos(playUrl, chapter.chapter_index));
-        } catch (error) {
-            console.error('Error:', error);
+    const limitedMergeVideos = limit(async (playUrl, chapterIndex) => {
+        return mergeVideos(playUrl, chapterIndex);
+    }, 5); 
+
+    async function loadVideoList(filmId, chapterData, channel, namaFilm) {
+        if(channel=='shortwave'){
+            chapterData = chapterData.result;
         }
-    });
+        console.log(chapterData)
+        const promises = chapterData.map(async (chapter) => {
+            console.log(chapter.chapter_id)
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: `http://localhost/shortwave/generatechunk.php?drama_id=${filmId}&chapter_id=${chapter.chapter_id}`,
+                headers: {
+                }
+            };
 
-    await Promise.all(promises);
-    console.log("DONE")
+            try {
+                const response = await axios.request(config);
+                const playUrl = response.data.data.play_url;
+
+                return limitedMergeVideos(() => mergeVideos(playUrl, chapter.chapter_index, channel, namaFilm, filmId));
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        });
+
+        await Promise.all(promises);
+        console.log("DONE")
+    }
+
+    socket.on('download', (data) => {
+        if (!data.err) {
+            if (data.channel == 'shortwave') {
+                loadVideoList(data.filmId, data.data, data.channel, data.namaFilm).catch(err => console.error('Error:', err));
+            }
+        }
+    })
+});
+
+async function ensureDirectoryExists(dirPath) {
+    try {
+        await fs.mkdir(dirPath, { recursive: true });
+    } catch (err) {
+        console.error('Error creating directory:', err);
+    }
 }
-
-// loadVideoList(data).catch(err => console.error('Error:', err));
--- router/shortwave.js
+++ router/shortwave.js
@@ -4,20 +4,26 @@ const funct = require('../function');
 const { randStr } = require('../function');
 const router = express.Router();
 
-var headers = {};
+
 
 router.get('/', async(req, res)=>{
-    await login();
+    // await login();
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: 'https://api.shortswave.com/v3/all?page=1&page_size=999',
-        headers: headers
+        url: 'http://localhost/shortwave/api.php?filmList',
+        headers: {}
       };
       
       axios.request(config)
       .then((response) => {
-        res.json({err:false, result:response.data})
+        var data = response.data;
+        if(data.err){
+            res.json({err:true, result:data});
+        }else{
+            // console.log(data)
+            res.render('shortwave/index', {chapters:data.data});
+        }
       })
       .catch((error) => {
         res.json({err:true, msg:error.message})
@@ -25,13 +31,12 @@ router.get('/', async(req, res)=>{
 })
 
 router.get('/:id', async (req, res) => {
-    await login();
     let id = req.params.id;
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: 'https://api.shortswave.com/v1/drama/chapter/list?drama_id='+id+'&page=1&page_size=999',
-        headers: headers
+        url: 'http://localhost/shortwave/api.php?drama_id='+id+'&list=1',
+        headers: {}
       };
       
       axios.request(config)
@@ -44,86 +49,86 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.get('/:id/:chapterid', async (req, res) => {
-    await login();
-    let id = req.params.id;
-    let chapterid = req.params.chapterid;
-    let config = {
-        method: 'get',
-        maxBodyLength: Infinity,
-        url: 'https://api.shortswave.com/v1/drama/chapter/detail?drama_id='+id+'&chapter_id='+chapterid+'&auto_pay=1',
-        headers: headers
-      };
+// router.get('/:id/:chapterid', async (req, res) => {
+//     await login();
+//     let id = req.params.id;
+//     let chapterid = req.params.chapterid;
+//     let config = {
+//         method: 'get',
+//         maxBodyLength: Infinity,
+//         url: 'https://api.shortswave.com/v1/drama/chapter/detail?drama_id='+id+'&chapter_id='+chapterid+'&auto_pay=1',
+//         headers: headers
+//       };
       
-      axios.request(config)
-      .then((response) => {
-        res.json({err:false, result:response.data})
-      })
-      .catch((error) => {
-        res.json({err:true, msg:error.message})
-      });
-});
+//       axios.request(config)
+//       .then((response) => {
+//         res.json({err:false, result:response.data})
+//       })
+//       .catch((error) => {
+//         res.json({err:true, msg:error.message})
+//       });
+// });
 
-async function login() {
-    let data = JSON.stringify({
-        "device_type": "Unk",
-        "attribution_type": "af",
-        "attribution_id": funct.randNum(13) + "-" + funct.randNum(7)
-    });
+// async function login() {
+//     let data = JSON.stringify({
+//         "device_type": "Unk",
+//         "attribution_type": "af",
+//         "attribution_id": funct.randNum(13) + "-" + funct.randNum(7)
+//     });
 
-    let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://api.shortswave.com/login/anonymous',
-        headers: { 
-            'app': 'com.company.shortsdrama.wave', 
-            'Device-UUid': '' + randStr(8).toUpperCase() + '-' + randStr(4).toUpperCase() + '-' + randStr(4).toUpperCase() + '-' + randStr(4).toUpperCase() + '-' + randStr(12).toUpperCase() + '+' + randStr(32).toUpperCase(), 
-            'Content-Type': 'application/json'
-        },
-        data: data
-    };
+//     let config = {
+//         method: 'post',
+//         maxBodyLength: Infinity,
+//         url: 'https://api.shortswave.com/login/anonymous',
+//         headers: { 
+//             'app': 'com.company.shortsdrama.wave', 
+//             'Device-UUid': '' + randStr(8).toUpperCase() + '-' + randStr(4).toUpperCase() + '-' + randStr(4).toUpperCase() + '-' + randStr(4).toUpperCase() + '-' + randStr(12).toUpperCase() + '+' + randStr(32).toUpperCase(), 
+//             'Content-Type': 'application/json'
+//         },
+//         data: data
+//     };
 
-    try {
-        const response = await axios.request(config);
+//     try {
+//         const response = await axios.request(config);
         
-        if (response.data && response.data.code === 0) {
-            let result = response.data.data;
-            headers = config.headers
-            headers['X-SESSION-TOKEN'] = result.session_token; // Store the session token in headers
-            await taskReward('1001');
-            await taskReward('1002');
-            await taskReward('1003');
-            return { err: false, res: response.data }; // Return the successful response
-        } else {
-            return { err: true, msg: 'Unexpected response code' };
-        }
-    } catch (error) {
-        return { err: true, msg: error.message }; // Return the error message
-    }
-}
+//         if (response.data && response.data.code === 0) {
+//             let result = response.data.data;
+//             headers = config.headers
+//             headers['X-SESSION-TOKEN'] = result.session_token; // Store the session token in headers
+//             await taskReward('1001');
+//             await taskReward('1002');
+//             await taskReward('1003');
+//             return { err: false, res: response.data }; // Return the successful response
+//         } else {
+//             return { err: true, msg: 'Unexpected response code' };
+//         }
+//     } catch (error) {
+//         return { err: true, msg: error.message }; // Return the error message
+//     }
+// }
 
 
-async function taskReward(taskId){
-    let data = JSON.stringify({
-        "task_id": taskId
-    });
+// async function taskReward(taskId){
+//     let data = JSON.stringify({
+//         "task_id": taskId
+//     });
 
-    let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://api.shortswave.com/task/submit',
-        headers: headers,
-        data : data
-    };
-    let rez = {
-        taskId:taskId
-    }
-    axios.request(config).then((response) => {
-        rez.data = response.data;
-        console.log(JSON.stringify(rez));
-    }).catch((error) => {
-        console.log(error);
-    });
-}
+//     let config = {
+//         method: 'post',
+//         maxBodyLength: Infinity,
+//         url: 'https://api.shortswave.com/task/submit',
+//         headers: headers,
+//         data : data
+//     };
+//     let rez = {
+//         taskId:taskId
+//     }
+//     axios.request(config).then((response) => {
+//         rez.data = response.data;
+//         console.log(JSON.stringify(rez));
+//     }).catch((error) => {
+//         console.log(error);
+//     });
+// }
 
 module.exports = router;
